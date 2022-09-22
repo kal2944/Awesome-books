38d91d3ff0ea7ed7566fd082b839fd02963c0538
kalkidan Daniel <99416452+kal2944@users.noreply.github.com>
2022-09-21 18:16:06 -0700

    add html and js

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # Awesome-books
+
+"Awesome books" is a simple website that displays a list of books and allows you to add and remove books from that list. 
+This Project will manage book collection using plain JS with objects. with this application, books can be added and removed.
+
+## Built With
+
+- HTML
+- JavaScript
+- CSS
+
+## Authors
+👤 **kalkidan Daniel**
+
+- GitHub: [@kal2944](https://github.com/kal2944)
+- Twitter: [@Kalkida01088197](https://twitter.com/Kalkida01088197)
+- LinkedIn: [@kalkidan Daniel](https://www.linkedin.com/in/kalkidan-daniel-b2a204238/)
+
+👤 **Erick Hans Martinez**
+
+- GitHub: [@erickhansx](https://github.com/erickhansx)
+- Twitter: [@TheErickHans](https://twitter.com/TheErickHans)
+- LinkedIn: [@erickhans](https://www.linkedin.com/in/erick-hans-858382231/)
+
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Microverse
--- src/index.js
+++ src/index.js
@@ -1,1 +1,52 @@
-console.log('kali');
+let booksArr = [];
+const form = document.querySelector('.insertBookForm');
+const bTitle = document.querySelector('#b-title');
+const bAuthor = document.querySelector('#b-author');
+
+function fetchBooks() {
+  const books = JSON.parse(localStorage.getItem('books'));
+  if (books !== null) {
+    booksArr = books;
+  }
+}
+
+function showBooks() {
+  const bookDiv = document.querySelector('.books');
+  let booksContent = '';
+  if (booksArr.length !== 0) {
+    booksArr.forEach((book) => {
+      booksContent += `<p>${book.title}</p> <p>${book.author}</p><button data-title=${book.title} class="deleteBook">Remove</button> <hr>`;
+    });
+    bookDiv.innerHTML = booksContent;
+    const deleteBook = document.querySelectorAll('.deleteBook');
+    deleteBook.forEach((element) => {
+      element.addEventListener('click', function deleteBook() {
+        const { title } = this.dataset;
+        const remove = booksArr.filter((b) => b.title !== title);
+        localStorage.setItem('books', JSON.stringify(remove));
+        fetchBooks();
+        showBooks();
+      });
+    });
+  } else {
+    bookDiv.innerHTML = '';
+  }
+}
+
+function insertBook(e) {
+  const book = {
+    title: bTitle.value,
+    author: bAuthor.value,
+  };
+  booksArr.push(book);
+  localStorage.setItem('books', JSON.stringify(booksArr));
+  e.preventDefault();
+  bTitle.value = '';
+  bAuthor.value = '';
+  showBooks();
+}
+
+fetchBooks();
+showBooks();
+
+form.addEventListener('submit', insertBook);
